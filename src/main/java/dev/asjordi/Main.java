60df41c782264ea63e7f58ca93bb11b49ddd86a9
Jordi Ayala <jord7as@gmail.com>
2024-07-08 13:27:01 -0600

    Initial commit

---
+++ src/main/java/dev/asjordi/Main.java
@@ -0,0 +1,13 @@
+
+package dev.asjordi;
+
+/**
+ *
+ * @author Jordi <ejordi.ayala@gmail.com>
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Java wc");
+    }
+}
